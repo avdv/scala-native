5dc814259316c144065dbab5a449b2d1b8f853d6
Claudio Bley <claudio.bley@gmail.com>
2019-07-07 23:00:11 +0200

    Simplify code a bit
    * make `getUsername` and `getGroupName` return a String instead of an Option

--- javalib/src/main/scala/scala/scalanative/nio/fs/attribute/NativeUserPrincipalLookupService.scala
+++ javalib/src/main/scala/scala/scalanative/nio/fs/attribute/NativeUserPrincipalLookupService.scala
@@ -9,23 +9,17 @@ import scalanative.posix.{errno => e, grp, pwd, unistd, time, utime}, e._
 import scalanative.posix.sys.stat
 import scala.scalanative.nio.fs.UnixException
 
-final case class NativeUserPrincipal(uid: stat.uid_t)(name: Option[String]) extends UserPrincipal {
+final case class NativeUserPrincipal(uid: stat.uid_t)(name: Option[String])
+    extends UserPrincipal {
   override def getName = {
-    name orElse {
-      NativeUserPrincipalLookupService.getUsername(uid)
-    } getOrElse {
-      uid.toString
-    }
+    name getOrElse NativeUserPrincipalLookupService.getUsername(uid)
   }
 }
 
-final case class NativeGroupPrincipal(gid: stat.gid_t)(name: Option[String]) extends GroupPrincipal {
+final case class NativeGroupPrincipal(gid: stat.gid_t)(name: Option[String])
+    extends GroupPrincipal {
   override def getName: String = {
-    name orElse {
-      NativeUserPrincipalLookupService.getGroupName(gid)
-    } getOrElse {
-      gid.toString
-    }
+    name getOrElse NativeUserPrincipalLookupService.getGroupName(gid)
   }
 }
 
@@ -44,38 +38,40 @@ object NativeUserPrincipalLookupService extends UserPrincipalLookupService {
       NativeGroupPrincipal(gid)(Some(group))
     }
 
+  private[attribute] def getGroupName(gid: stat.gid_t): String = Zone {
+    implicit z =>
+      val buf = alloc[grp.group]
 
-  private[attribute] def getGroupName(gid: stat.gid_t): Option[String] = Zone { implicit z =>
-    val buf = alloc[grp.group]
+      errno.errno = 0
+      val err = grp.getgrgid(gid, buf)
 
-    errno.errno = 0
-    val err = grp.getgrgid(gid, buf)
-
-    if (err == 0) {
-      Some(fromCString(buf._1))
-    } else if (errno.errno == 0) {
-      None
-    } else {
-      throw UnixException("getgrgid", errno.errno)
-    }
+      if (err == 0) {
+        fromCString(buf._1)
+      } else if (errno.errno == 0) {
+        gid.toString
+      } else {
+        throw UnixException("getgrgid", errno.errno)
+      }
   }
 
-  private[attribute] def getUsername(uid: stat.uid_t): Option[String] = Zone { implicit z =>
-    val buf = alloc[pwd.passwd]
+  private[attribute] def getUsername(uid: stat.uid_t): String = Zone {
+    implicit z =>
+      val buf = alloc[pwd.passwd]
 
-    errno.errno = 0
-    val err = pwd.getpwuid(uid, buf)
+      errno.errno = 0
+      val err = pwd.getpwuid(uid, buf)
 
-    if (err == 0) {
-      Some(fromCString(buf._1))
-    } else if (errno.errno == 0) {
-      None
-    } else {
-      throw UnixException("getpwuid", errno.errno)
-    }
+      if (err == 0) {
+        fromCString(buf._1)
+      } else if (errno.errno == 0) {
+        uid.toString
+      } else {
+        throw UnixException("getpwuid", errno.errno)
+      }
   }
 
-  private def getGroup(name: CString)(implicit z: Zone): Option[Ptr[grp.group]] = {
+  private def getGroup(name: CString)(
+      implicit z: Zone): Option[Ptr[grp.group]] = {
     val buf = alloc[grp.group]
 
     errno.errno = 0
@@ -101,10 +97,11 @@ object NativeUserPrincipalLookupService extends UserPrincipalLookupService {
         }
       }(_._2)
 
-    NativeUserPrincipal(uid)(Some(name))
+      NativeUserPrincipal(uid)(Some(name))
   }
 
-  private def getPasswd(name: CString)(implicit z: Zone): Option[Ptr[pwd.passwd]] = {
+  private def getPasswd(name: CString)(
+      implicit z: Zone): Option[Ptr[pwd.passwd]] = {
     val buf = alloc[pwd.passwd]
 
     errno.errno = 0
